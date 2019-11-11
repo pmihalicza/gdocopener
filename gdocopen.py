128c4c17996823af967b0d168cc376748f25b03a
Peti <peter.mihalicza@gmail.com>
2019-11-11 15:11:58 +0100

    Implement and test url open functionality under Linux
    Next steps:
    - implement URL extraction from gdoc JSON

--- gdocopen.py
+++ gdocopen.py
@@ -1,13 +1,23 @@
 from sys import argv
+from subprocess import call
+import platform
 
 def extract_url_from_gdoc(gdoc_file):
     pass
 
-def open_url_in_chrome(gdoc_path):
-    command = "google-chrome".join(gdoc_path)
-    run(command)
+def open_url_in_chrome(gdoc_url):
+    '''This function takes the url to the gdoc file and opens it in the Chrome browser.
+    The function chooses the appropriate command to start Chrome based on the user's current OS'''
+
+    if platform.system() == 'Linux':
+        command = 'google-chrome'
+    elif platform.system() == 'Windows':
+        command = 'Chrome.exe'
+
+    call([command, gdoc_url])
 
 if __name__ == '__main__':
     gdoc_file = argv[1]
     gdoc_path = extract_url_from_gdoc(gdoc_file=gdoc_file)
+
     open_url_in_chrome(gdoc_path)
---
+++ test_gdocopen.py
@@ -0,0 +1,6 @@
+import gdocopen as gd
+
+if __name__ == '__main__':
+
+    gdoc_url = 'https://imdb.com'
+    gd.open_url_in_chrome(gdoc_url)
