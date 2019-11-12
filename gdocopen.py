5eb7527bb4f70a0a2cab464172c925a388a7e0b7
Peti <peter.mihalicza@gmail.com>
2019-11-12 10:35:38 +0100

    Implement and test URL extraction from gdoc JSON
    This is the first working version.
    
    Next steps:
    - Create Linux desktop application file and test if it works as expected
    - Error handling

--- gdocopen.py
+++ gdocopen.py
@@ -1,13 +1,21 @@
 from sys import argv
 from subprocess import call
 import platform
+import json
 
 def extract_url_from_gdoc(gdoc_file):
-    pass
+    """This function opens the gdoc_file, reads its JSON contents and parses it. It then extracts and returns the URL to
+    the Docs file."""
+    file = open(gdoc_file, 'r')
+    content = file.read()
+    file.close()
+
+    json_dict = json.loads(content)
+    return json_dict['url']
 
 def open_url_in_chrome(gdoc_url):
-    '''This function takes the url to the gdoc file and opens it in the Chrome browser.
-    The function chooses the appropriate command to start Chrome based on the user's current OS'''
+    """This function takes the url of the gdoc file and opens it in the Chrome browser.
+    The function chooses the appropriate command to start Chrome based on the user's current OS."""
 
     if platform.system() == 'Linux':
         command = 'google-chrome'
