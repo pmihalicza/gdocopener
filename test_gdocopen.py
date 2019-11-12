3762ee9a73d07402e3b348c0da3850c45dad81bd
Peti <peter.mihalicza@gmail.com>
2019-11-12 11:53:03 +0100

    Create Linux desktop application file
    - Add test file with masked dropbox URL

--- test_gdocopen.py
+++ test_gdocopen.py
@@ -4,3 +4,6 @@ if __name__ == '__main__':
 
     gdoc_url = 'https://imdb.com'
     gd.open_url_in_chrome(gdoc_url)
+
+    gdoc_file = '/home/peti/Dropbox/test_gdoc_file.gdoc'
+    print(gd.extract_url_from_gdoc(gdoc_file) == 'https://www.dropbox.com/cloud_docs/view/#############')
