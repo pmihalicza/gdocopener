6963b53008f080372815f8ce16d054f9e1ef9669
Peti <peter.mihalicza@gmail.com>
2019-11-12 13:34:10 +0100

    Create single executable on Linux with Pyinstaller

--- test_gdocopen.py
+++ test_gdocopen.py
@@ -1,9 +1,15 @@
 import gdocopen as gd
+import unittest
+
+class MyTestCLass(unittest.TestCase):
+    def test_open_url_in_chrome(self):
+        gdoc_url = 'https://imdb.com'
+        gd.open_url_in_chrome(gdoc_url)
+
+        self.assertEqual()
 
 if __name__ == '__main__':
 
-    gdoc_url = 'https://imdb.com'
-    gd.open_url_in_chrome(gdoc_url)
 
     gdoc_file = '/home/peti/Dropbox/test_gdoc_file.gdoc'
-    print(gd.extract_url_from_gdoc(gdoc_file) == 'https://www.dropbox.com/cloud_docs/view/#############')
+    print(gd.extract_url_from_gdoc(gdoc_file) == 'httnps://www.dropbox.com/cloud_docs/view/#############')
