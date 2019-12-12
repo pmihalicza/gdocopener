e1206bb7596b9f0013176982f42c3609a77f3a53
Peti <peter.mihalicza@gmail.com>
2019-12-12 08:51:36 +0100

    Add error checking a user feedback
    - Check for non-existing files
    - Check for non-JSON files

--- gdocopen.py
+++ gdocopen.py
@@ -6,12 +6,23 @@ import json
 def extract_url_from_gdoc(gdoc_file):
     """This function opens the gdoc_file, reads its JSON contents and parses it. It then extracts and returns the URL to
     the Docs file."""
-    file = open(gdoc_file, 'r')
-    content = file.read()
-    file.close()
+    try:
+        with open(gdoc_file, 'r') as file:
+            content = file.read()
 
-    json_dict = json.loads(content)
-    return json_dict['url']
+            json_dict = json.loads(content)
+            return json_dict['url']
+
+    except FileNotFoundError:
+        error = 'The file you want to open does not exist!'
+        print(error)
+        return error
+
+    except json.JSONDecodeError:
+        error = 'The file you want to open is not a valid gdoc file (aka JSON file)!'
+        print(error)
+        return error
+
 
 def open_url_in_chrome(gdoc_url):
     """This function takes the url of the gdoc file and opens it in the Chrome browser.
@@ -29,7 +40,10 @@ def open_url_in_chrome(gdoc_url):
         return 1
 
 if __name__ == '__main__':
-    gdoc_file = argv[1]
-    gdoc_path = extract_url_from_gdoc(gdoc_file=gdoc_file)
+    if len(argv) > 1:
+        gdoc_file = argv[1]
+        gdoc_path = extract_url_from_gdoc(gdoc_file=gdoc_file)
 
-    open_url_in_chrome(gdoc_path)
+        open_url_in_chrome(gdoc_path)
+    else:
+        print('You should pass a file as an argument to gdocopen!')
--- test_gdocopen.py
+++ test_gdocopen.py
@@ -1,4 +1,5 @@
 import gdocopen as gd
+import pytest
 
 def test_open_url_in_chrome():
     gdoc_url = 'https://imdb.com'
@@ -7,5 +8,18 @@ def test_open_url_in_chrome():
 
 def test_extract_url_from_gdoc():
     gdoc_file = '/home/peti/Dropbox/test_gdoc_file.gdoc'
-    assert gd.extract_url_from_gdoc(gdoc_file) == 'https://www.dropbox.com/cloud_docs/view/#############', \
-        'THe URL extracted is not right'
+    extracted_url = gd.extract_url_from_gdoc(gdoc_file)
+    assert extracted_url == 'https://www.dropbox.com/cloud_docs/view/#############', \
+        'THe extracted URL  is not correct'
+
+def test_extract_url_from_gdoc_with_nonJSON_file():
+    gdoc_file = '/home/peti/Dropbox/randomfile.txt'
+    extracted_url = gd.extract_url_from_gdoc(gdoc_file)
+
+    assert extracted_url == 'The file you want to open is not a valid gdoc file (aka JSON file)!', 'NonJSON files are not handled correctly'
+
+def test_extract_url_from_doc_with_nonexisting_file():
+    gdoc_file = '/home/peti/Dropbox/thisdoesnotexists.txt'
+    extracted_url = gd.extract_url_from_gdoc(gdoc_file)
+
+    assert extracted_url == 'The file you want to open does not exist!', 'Not existing files are not handled correctly'
